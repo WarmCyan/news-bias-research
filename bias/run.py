2408840a8238b1f259196cbc134092a5e62597c9
Nathan Martindale <nathanamartindale@gmail.com>
2019-10-21 15:51:04 -0500

    Create notebook for experimenting with CNN models

--- bias/run.py
+++ bias/run.py
@@ -9,7 +9,7 @@ def create_datasets():
 
     overwrite_w2v = False
     overwrite_glove = False
-    overwrite_tfidf = True
+    overwrite_tfidf = False
 
     datasets.create_word2vec(df_os_reliability, "os_reliable_w2v", overwrite=overwrite_w2v)
     datasets.create_word2vec(df_mbfc_reliability, "mbfc_reliable_w2v", overwrite=overwrite_w2v)
@@ -20,21 +20,13 @@ def create_datasets():
     datasets.create_glove(df_os_reliability, "os_reliable_glove", overwrite=overwrite_glove)
     datasets.create_glove(df_mbfc_reliability, "mbfc_reliable_glove", overwrite=overwrite_glove)
     datasets.create_glove(df_ng_reliability, "ng_reliable_glove", overwrite=overwrite_glove)
-    
+
     datasets.clear_vector_model()
-    
+
     datasets.create_tfidf(df_os_reliability, "os_reliable_tfidf", overwrite=overwrite_tfidf)
     datasets.create_tfidf(df_mbfc_reliability, "mbfc_reliable_tfidf", overwrite=overwrite_tfidf)
     datasets.create_tfidf(df_ng_reliability, "ng_reliable_tfidf", overwrite=overwrite_tfidf)
 
-    # datasets.create_word2vec(df_os_reliability, "os_reliable_glove_sequence", shaping="sequence")
-    # datasets.create_word2vec(df_os_reliability, "os_reliable_glove_avg", shaping="avg")
-    # 
-    # datasets.create_word2vec(df_mbfc_reliability, "mbfc_reliable_glove_sequence", shaping="sequence")
-    # datasets.create_word2vec(df_mbfc_reliability, "mbfc_reliable_glove_avg", shaping="avg")
-    # 
-    # datasets.create_word2vec(df_ng_reliability, "ng_reliable_glove_sequence", shaping="sequence")
-    # datasets.create_word2vec(df_ng_reliability, "ng_reliable_glove_avg", shaping="avg")
 
 if __name__ == "__main__":
     util.init_logging()
