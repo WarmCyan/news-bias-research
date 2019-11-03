fc5ce68a5e0fe520ce613588c3a710d4ff0a3dd2
Nathan Martindale <nathanamartindale@gmail.com>
2019-11-03 11:47:38 -0600

    Add script to write scripts for running experiments from experiment file

--- jobs/generate.py
+++ jobs/generate.py
@@ -1,12 +1,19 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-n", dest="name", default=None, required=True, help="name")
+parser.add_argument("-n", dest="name", default=None, help="name")
 parser.add_argument("-e", dest="experiment_path", default=None, help="experiment file name", required=True)
 parser.add_argument("-r", dest="experiment_row", default=None, help="row")
 parser.add_argument("-m", dest="memory", default="32000", help="memory (32000)")
 parser.add_argument("-t", dest="time", default="1:00:00", help="time (1:00:00)")
 args = parser.parse_args()
+
+name = args.name
+
+if name is None:
+    name = args.experiment_path
+    if args.experiment_row is not None:
+        name += "_" + args.experiment_row
 
 common_lines = [
     "#!/bin/bash",
@@ -18,17 +25,17 @@ common_lines = [
     "cd /home/tntech.edu/namartinda42/research/bias"
 ]
 
-experiment_line = "python experiment.py --experiment ../experiments/" + args.experiment_path
+experiment_line = "python experiment.py --experiment ../experiments/" + args.experiment_path + ".json"
 if args.experiment_row is not None:
     experiment_line += " --row " + args.experiment_row
-experiment_line += " --log $LOGPATH/" + args.name + ".log"
+experiment_line += " --log $LOGPATH/" + name + ".log"
 
 common_lines.append(experiment_line)
 
 for i in range(0, len(common_lines)):
     common_lines[i] += "\n"
 
-with open(args.name + '.sh', 'w') as outfile:
+with open(name + '.sh', 'w') as outfile:
     outfile.writelines(common_lines)
 
-print(args.name + ".sh")
+print(name + ".sh")
