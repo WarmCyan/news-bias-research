d30681578ccfd6fc8e4fc4644cc7e4bcd4e20edd
Nathan Martindale <nathanamartindale@gmail.com>
2019-12-12 15:05:29 -0600

    Create new lstm experiment batch

--- jobs/lstm_bias_0_2.sh
+++ jobs/lstm_bias_0_2.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH --mem=96000
 #SBATCH --time=6:00:00
+TMP=$(mktemp -d)
 cd /home/tntech.edu/namartinda42/research
 . env_setup.sh
 . jobs/common.sh
 cd /home/tntech.edu/namartinda42/research/bias
-python experiment.py --experiment ../experiments/lstm_bias_0.json --row 2 --log $LOGPATH/lstm_bias_0_2.log
+python experiment.py --experiment ../experiments/lstm_bias_0.json --temp $TMP --row 2 --log $LOGPATH/lstm_bias_0_2.log
