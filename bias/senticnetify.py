2cae2c74fc4da50d12f041fd4e08d15798d0213f
Nathan Martindale <nathanamartindale@gmail.com>
2020-01-13 11:53:33 -0600

    Add pattern finding

--- bias/senticnetify.py
+++ bias/senticnetify.py
@@ -14,10 +14,41 @@ patterns = [
 ]
 
 
-
-def find_tagged(tagged_words):
+def find_tagged(tagged_words, pattern):
+    found_patterns = []
+    end = len(tagged_words) - len(pattern)
     for index, (word, pos) in enumerate(tagged_words, 1):
-        pass       
+
+        if index == end:
+            break
+
+        i = 0
+        found = True
+        
+        for part in pattern:
+            if part != tagged_words[index + i][1]:
+                found = False
+                break
+            i += 1
+
+        if found:
+            found_pattern = tagged_words[index:index + len(pattern)]
+            found_patterns.append(found_pattern)
+
+    return found_patterns
+        
+
+def get_sentiment_vector_instance(sentiment):
+    pv = sentiment["polarity_value"]
+    pi = sentiment["polarity_intense"]
+    spl = sentiment["sentics"]["pleasentness"]
+    satt = sentiment["sentics"]["attention"]
+    ssen = sentiment["sentics"]["sensitivity"]
+    sapt = sentiment["sentics"]["aptitude"]
+    
+    sentic_vector_instance = [pv, pi, spl, satt, ssen, sapt]
+    return sentic_vector_instance
+
 
 def get_article_embedding(doc_words, model):
     global sn
@@ -29,38 +60,46 @@ def get_article_embedding(doc_words, model):
     
     tagged = nltk.pos_tag(doc_words, tagset="universal")
 
+    patterns = find_tagged(tagged, patterns[0])
+    patterns.extend(find_tagged(tagged, patterns[1]))
+    patterns.extend(find_tagged(tagged, patterns[2]))
+    patterns.extend(find_tagged(tagged, patterns[3]))
+
     sentic_vectors = []
     
-    # TODO: find patterns and words to check with senticnet
-
-    for pattern in tagged: # change latter portion
+    for pattern in patterns: # change latter portion
         pattern_text = ""
         pattern_words = []
         for word in pattern:
             pattern_text += word[0]
             pattern_words.append(word[0])
+
+        sentic_vector_instances = []
+        
         try: 
             sentiment = sn.concept(pattern_text)
+            sentic_vector_instances.append(get_sentiment_vector_instance(sentiment)
         except KeyError: 
-            # if not, try individual words in pattern
-            continue
-        
-        pv = sentiment["polarity_value"]
-        pi = sentiment["polarity_intense"]
-        spl = sentiment["sentics"]["pleasentness"]
-        satt = sentiment["sentics"]["attention"]
-        ssen = sentiment["sentics"]["sensitivity"]
-        sapt = sentiment["sentics"]["aptitude"]
-
-        sentic_vector = [pv, pi, spl, satt, ssen, sapt]
+            for word in pattern_words:
+                try:
+                    sentiment = sn.concept(word)
+                    sentic_vector_instances.append(get_sentiment_vector_instance(sentiment)
+                except KeyError: 
+                    sentic_vector_instances.append([0,0,0,0,0,0])
+                    continue
+                
 
         if model is not None:
-            for word in pattern_words:
+            for index, word in enumerate(pattern_words):
                 vector = []
-                vector.extend(sentic_vector)
+                if len(sentic_vector_instances) > 1:
+                    vector.extend(sentic_vector_instances[index])
+                else:
+                    vector.extend(sentic_vector_instances[0])
+                
                 vector.extend(model[word])
                 sentic_vectors.append(vector)
         else:
-            sentic_vectors.append(sentic_vector)
+            sentic_vectors.extend(sentic_vector_instances)
 
     return sentic_vectors
